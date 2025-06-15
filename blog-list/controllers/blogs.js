187b89ba74760dd3372232c741b7238ef2738c1d
lucksei <luca6mandi@gmail.com>
2025-06-15 04:40:00 -0300

    excercise 4.13 done (Blog List Expansions, step 1)

--- blog-list/controllers/blogs.js
+++ blog-list/controllers/blogs.js
@@ -1,27 +1,39 @@
 const blogsRouter = require('express').Router()
+const { request } = require('../app')
 const blog = require('./../models/blog')
 const Blog = require('./../models/blog')
+const logger = require('./../utils/logger')
 
 blogsRouter.get('/', async (request, response, next) => {
-    const blogs = await Blog.find({})
-    response.json(blogs)
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-    const blog = new Blog({
-        title: request.body.title,
-        author: request.body.author,
-        url: request.body.url,
-        likes: request.body.likes
-    })
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes
+  })
 
 
-    try {
-        const savedBlog = await blog.save()
-        response.status(201).json(savedBlog)
-    } catch (exeption) {
-        next(exeption)
-    }
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch (exeption) {
+    next(exeption)
+  }
+})
+
+blogsRouter.delete('/:blogId', async (request, response, next) => {
+  const { blogId } = request.params
+  try {
+    await Blog.findByIdAndDelete(blogId)
+    response.status(204).end()
+  } catch (exeption) {
+    next(exeption)
+  }
 })
 
 module.exports = blogsRouter
--- blog-list/tests/blog_list.test.js
+++ blog-list/tests/blog_list.test.js
@@ -1,4 +1,4 @@
-const {test, after, beforeEach} = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const assert = require('assert')
@@ -50,100 +50,119 @@ const initialBlogs = [
 // The database is cleared out at the beginning, after that we save
 // two notes stored in the initialBlogs array to the database.
 beforeEach(async () => {
-    await Blog.deleteMany({})
-    const blogObjects = initialBlogs.map(blog => new Blog(blog))
-    const promiseArray = blogObjects.map(blog => blog.save())
-    await Promise.all(promiseArray)
+  await Blog.deleteMany({})
+  const blogObjects = initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
 })
 
 test('blogs are returned as json', async () => {
-    await api
+  await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
 test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
+  const response = await api.get('/api/blogs')
 
-    assert.strictEqual(response.body.length, initialBlogs.length)
+  assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
 test('verify that the unique identifier is named id', async () => {
-    const response = await api.get('/api/blogs')
-    response.body.forEach((blog) => {
-        // console.log(Object.keys(blog).includes("id"))
-        assert(Object.keys(blog).includes("id"))
-    })
+  const response = await api.get('/api/blogs')
+  response.body.forEach((blog) => {
+    // console.log(Object.keys(blog).includes("id"))
+    assert(Object.keys(blog).includes("id"))
+  })
 })
 
 test('verify that making a POST request to /api/blogs creates a new blog post', async () => {
-    const newBlog = {
-        title: "New test blog",
-        author: "D",
-        url: "https://localhost/",
-        likes: 999,
-    }
+  const newBlog = {
+    title: "New test blog",
+    author: "D",
+    url: "https://localhost/",
+    likes: 999,
+  }
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect("Content-Type", /application\/json/)
-    
-    const response = await api.get('/api/blogs')
-    const titles = response.body.map(b => b.title)
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/)
 
-    assert.strictEqual(response.body.length, initialBlogs.length + 1)
-    assert(titles.includes("New test blog"))
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(b => b.title)
+
+  assert.strictEqual(response.body.length, initialBlogs.length + 1)
+  assert(titles.includes("New test blog"))
 })
 
 test('verify that if the "likes" property is missing from the POST request, it will default to the valie 0', async () => {
-    const newBlog = {
-        title: "Missing likes property",
-        author: "D",
-        url: "https://localhost/",
-    }
+  const newBlog = {
+    title: "Missing likes property",
+    author: "D",
+    url: "https://localhost/",
+  }
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect("Content-Type", /application\/json/)
-    
-    const response = await api.get('/api/blogs')
-    const lastBlogSaved = _.find(response.body, blog => blog.title === "Missing likes property")
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/)
 
-    assert(Object.keys(lastBlogSaved).includes("likes"))
+  const response = await api.get('/api/blogs')
+  const lastBlogSaved = _.find(response.body, blog => blog.title === "Missing likes property")
+
+  assert(Object.keys(lastBlogSaved).includes("likes"))
 
 })
 
 test('verify that if the "title" property is missing from the POST request, it will return with code 400', async () => {
-    const newBlog = {
-        author: "D",
-        url: "https://localhost/",
-        likes: 9,
-    }
+  const newBlog = {
+    author: "D",
+    url: "https://localhost/",
+    likes: 9,
+  }
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
 })
 
 test('verify that if the "url" property is missing from the POST request, it will return with code 400', async () => {
-    const newBlog = {
-        title: "Missing URL",
-        author: "D",
-        likes: 9,
-    }
+  const newBlog = {
+    title: "Missing URL",
+    author: "D",
+    likes: 9,
+  }
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
 })
 
+test('verify that deleting a note results in code 204 and the note no longer exists', async () => {
+  let response = await api
+    .get('/api/blogs')
+    .expect(200)
+
+  const noteToDelete = _.find(response.body, blog => blog.title === "Go To Statement Considered Harmful")
+  await api
+    .delete(`/api/blogs/${noteToDelete.id}`)
+    .expect(204)
+
+  response = await api
+    .get('/api/blogs')
+    .expect(200)
+  const noteDeleted = _.find(response.body, blog => blog.title === "Go To Statement Considered Harmful")
+  assert(!noteDeleted)
+})
+
+
+test()
 after(async () => {
-    await mongoose.connection.close()
+  await mongoose.connection.close()
 })
