69af4e27a7166e6ef5d5aeca5eaea40ad1866b2b
lucksei <luca6mandi@gmail.com>
2025-06-15 01:33:04 -0300

    excercise 4.8 done (Blog List Tests, step 1)

--- blog-list/controllers/blogs.js
+++ blog-list/controllers/blogs.js
@@ -1,10 +1,13 @@
 const blogsRouter = require('express').Router()
+const blog = require('./../models/blog')
 const Blog = require('./../models/blog')
 
-blogsRouter.get('/', (request, response, next) => {
-    Blog.find({})
-        .then((blogs) => { response.json(blogs) })
-        .catch(error => next(error))
+blogsRouter.get('/', async (request, response, next) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
+    // Blog.find({})
+    //     .then((blogs) => { response.json(blogs) })
+    //     .catch(error => next(error))
 })
 
 blogsRouter.post('/', (request, response, next) => {
---
+++ blog-list/tests/blog_list.test.js
@@ -0,0 +1,73 @@
+const {test, after, beforeEach} = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const assert = require('assert')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+  },
+  {
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+  },
+  {
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+  },
+  {
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+  }
+]
+
+// The database is cleared out at the beginning, after that we save
+// two notes stored in the initialBlogs array to the database.
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    const blogObjects = initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
+test('blogs are returned as json', async () => {
+    await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
