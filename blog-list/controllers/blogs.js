6239077a1e47afc5c50a3936620153cb4dcea1b1
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-06-18 00:05:41 -0300

    removed commented out code and rerun tests to see if everything was working fine, exercise 4.23 was about fixing tests that broke but i just did that along the way as i coded the new features so it also should be done, this should be if im not mistaken the last commit before submitting

--- blog-list/controllers/blogs.js
+++ blog-list/controllers/blogs.js
@@ -4,26 +4,12 @@ const Blog = require('./../models/blog')
 const User = require('./../models/user')
 const logger = require('./../utils/logger')
 
-// returns the authorization token from the header or null
-// const getTokenFrom = (request) => {
-//   const authorization = request.get('authorization')
-//   if (authorization && authorization.startsWith('Bearer ')) {
-//     return authorization.replace('Bearer ', '')
-//   }
-//   return null
-// }
-
 blogsRouter.get('/', async (request, response, next) => {
   const blogs = await Blog.find({}).populate('user')
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-  // const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  // if (!decodedToken.id) {
-  //   return response.status(401).json({ error: "token invalid" })
-  // }
-  // const user = await User.findById(decodedToken.id)
 
   if (!request.user) {
     return response.status(401).json({ error: 'user missing or not valid' })
@@ -49,11 +35,6 @@ blogsRouter.post('/', async (request, response, next) => {
 
 blogsRouter.delete('/:blogId', async (request, response, next) => {
   const { blogId } = request.params
-
-  // const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  // if (!decodedToken.id) {
-  //   return response.status(401).json({ error: "token invalid" })
-  // }
 
   if (!request.user) {
     return response.status(401).json({ error: 'user missing or not valid' })
